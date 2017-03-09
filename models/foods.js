12cfd80efa244d666b9350d29dab1223a609286e
Kowus <barnabas@elite-education.org>
2017-03-09 09:33:39 +0000

    added food collection

--- models/foods.js
+++ models/foods.js
@@ -1,3 +1,15 @@
-/**
- * Created by kowus on 3/9/17.
- */
+var mongoose =  require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var foodSchema = new Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},restaurants: [{
+			id: {type:Schema.Types.ObjectId}
+	}]
+});
+
+module.exports = mongoose.model('food', foodSchema);
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Restaurant = require('../models/restaurants.model');
+var Food = require('../models/foods');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
